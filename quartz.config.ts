9d5098520d03ec01e1111ca57200fb5e19b1767e
Postcount CI <postcount-ci@firegroup.io>
2024-01-24 13:04:54 +0700

    Quartz sync: Jan 24, 2024, 1:04 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "One Percent+",
+    pageTitle: "OnePercent+",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
